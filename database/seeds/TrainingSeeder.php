947b8b8d2459d88b8eb147e5b8c9a031cf5afe0c
yukawa-yukawa <yooosuke.0924@gmail.com>
2022-12-12 20:35:36 +0900

    TrainingSeeder入力完了

--- database/seeds/TrainingSeeder.php
+++ database/seeds/TrainingSeeder.php
@@ -34,5 +34,634 @@ class TrainingSeeder extends Seeder
             'equipment_id' => 1,
             'name_id' => 3,
         ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 4,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 4,
+            'name_id' => 5,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 6,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 7,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 4,
+            'name_id' => 8,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 4,
+            'name_id' => 9,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 2,
+            'equipment_id' => 0,
+            'name_id' => 10,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 0,
+            'name_id' => 11,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 3,
+            'equipment_id' => 0,
+            'name_id' => 12,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 4,
+            'equipment_id' => 0,
+            'name_id' => 13,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 0,
+            'name_id' => 14,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 0,
+            'name_id' => 15,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 2,
+            'equipment_id' => 2,
+            'name_id' => 16,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 2,
+            'name_id' => 17,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 1,
+            'part_id' => 4,
+            'equipment_id' => 2,
+            'name_id' => 18,
+        ]);
+
+        //男筋肥大終了
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 2,
+            'part_id' => 4,
+            'equipment_id' => 3,
+            'name_id' => 19,
+        ]);
+            
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 2,
+            'part_id' => 1,
+            'equipment_id' => 1,
+            'name_id' => 20,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 2,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 21,
+        ]);
+
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 2,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 22,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 2,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 23,
+        ]);
+
+        //男maxup終了
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 3,
+            'name_id' => 24,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 2,
+            'equipment_id' => 5,
+            'name_id' => 25,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 1,
+            'name_id' => 26,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 27,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 4,
+            'name_id' => 28,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 29,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 30,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 4,
+            'name_id' => 31,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 4,
+            'name_id' => 32,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 2,
+            'equipment_id' => 0,
+            'name_id' => 33,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 0,
+            'name_id' => 34,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 3,
+            'equipment_id' => 0,
+            'name_id' => 35,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 0,
+            'name_id' => 36,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 0,
+            'name_id' => 37,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 0,
+            'name_id' => 38,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 2,
+            'name_id' => 39,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 1,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 2,
+            'name_id' => 40,
+        ]);
+
+        //男ダイエット及び全男終了
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 4,
+            'equipment_id' => 3,
+            'name_id' => 41,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 2,
+            'equipment_id' => 5,
+            'name_id' => 42,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 1,
+            'name_id' => 43,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 44,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 4,
+            'name_id' => 45,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 46,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 47,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 4,
+            'name_id' => 48,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 4,
+            'name_id' => 49,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 2,
+            'equipment_id' => 0,
+            'name_id' => 50,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 0,
+            'name_id' => 51,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 3,
+            'equipment_id' => 0,
+            'name_id' => 52,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 4,
+            'equipment_id' => 0,
+            'name_id' => 53,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 5,
+            'equipment_id' => 0,
+            'name_id' => 54,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 6,
+            'equipment_id' => 0,
+            'name_id' => 55,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 2,
+            'equipment_id' => 2,
+            'name_id' => 56,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 1,
+            'equipment_id' => 2,
+            'name_id' => 57,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 1,
+            'part_id' => 4,
+            'equipment_id' => 2,
+            'name_id' => 58,
+        ]);
+
+        //筋肥大終了
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 2,
+            'part_id' => 4,
+            'equipment_id' => 3,
+            'name_id' => 59,
+        ]);
+            
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 2,
+            'part_id' => 1,
+            'equipment_id' => 1,
+            'name_id' => 60,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 2,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 61,
+        ]);
+
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 2,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 62,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 2,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 63,
+        ]);
+
+        //maxup終了
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 3,
+            'name_id' => 64,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 2,
+            'equipment_id' => 5,
+            'name_id' => 65,
+        ]);
+        
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 1,
+            'name_id' => 66,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 3,
+            'equipment_id' => 3,
+            'name_id' => 67,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 4,
+            'name_id' => 68,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 1,
+            'name_id' => 69,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 1,
+            'name_id' => 70,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 4,
+            'name_id' => 71,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 4,
+            'name_id' => 72,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 2,
+            'equipment_id' => 0,
+            'name_id' => 73,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 0,
+            'name_id' => 74,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 3,
+            'equipment_id' => 0,
+            'name_id' => 75,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 0,
+            'name_id' => 76,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 5,
+            'equipment_id' => 0,
+            'name_id' => 77,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 6,
+            'equipment_id' => 0,
+            'name_id' => 78,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 1,
+            'equipment_id' => 2,
+            'name_id' => 79,
+        ]);
+
+        DB::table('muscles')->insert([
+            'sex_id' => 2,
+            'objective_id' => 3,
+            'part_id' => 4,
+            'equipment_id' => 2,
+            'name_id' => 80,
+        ]);
+
+        //女ダイエット及び女全終了
     }
 }
